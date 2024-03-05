bc3b9f0f5a52512eb9d07c39d4b209df627e5494
Vladimir Aleshechkin <min6220@yandex.ru>
2024-03-05 21:11:16 +0500

    Релиз 1_2_1_0

---
+++ OneScriptFormsDesigner/OneScriptFormsDesigner/DataGridViewCellStyleHeaders.cs
@@ -0,0 +1,175 @@
+﻿using System.ComponentModel;
+using System.Drawing.Design;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace osfDesigner
+{
+    public class DataGridViewCellStyleHeaders : System.Windows.Forms.DataGridViewCellStyle
+    {
+
+        [Browsable(false)]
+        public System.Windows.Forms.DataGridViewCellStyle OriginalObj { get; set; }
+
+        public DataGridViewCellStyleHeaders(System.Windows.Forms.DataGridViewCellStyle p1) : base(p1)
+        {
+            OriginalObj = p1;
+        }
+
+        public DataGridViewCellStyleHeaders()
+        {
+        }
+
+        [DisplayName("Выравнивание")]
+        [Description("Возвращает или задает значение, показывающее положение содержимого в ячейке объекта Таблица (DataGridView).")]
+        [Category("Макет")]
+        [Browsable(true)]
+        public new DataGridViewContentAlignment Alignment
+        {
+            get { return (DataGridViewContentAlignment)base.Alignment; }
+            set { base.Alignment = (System.Windows.Forms.DataGridViewContentAlignment)value; }
+        }
+
+        [DisplayName("Заполнение")]
+        [Description("Возвращает или задает расстояние между краем ячейки и ее содержимым.")]
+        [Category("Макет")]
+        [Browsable(true)]
+        [TypeConverter(typeof(MyPaddingConverter))]
+        [Editor(typeof(MyPaddingEditor), typeof(UITypeEditor))]
+        public new Padding Padding
+        {
+            get { return base.Padding; }
+            set { base.Padding = value; }
+        }
+
+        [DisplayName("ОсновнойЦвет")]
+        [Description("Возвращает или задает основной цвет ячейки объекта Таблица (DataGridView).")]
+        [Category("Внешний вид")]
+        [Browsable(true)]
+        [TypeConverter(typeof(MyColorConverter))]
+        [Editor(typeof(MyColorEditor), typeof(UITypeEditor))]
+        public new Color ForeColor
+        {
+            get { return base.ForeColor; }
+            set { base.ForeColor = value; }
+        }
+
+        [DisplayName("ОсновнойЦветВыделенного")]
+        [Description("Возвращает или задает основной цвет, используемый ячейкой объекта Таблица (DataGridView), когда она выбрана.")]
+        [Category("Внешний вид")]
+        [Browsable(true)]
+        [TypeConverter(typeof(MyColorConverter))]
+        [Editor(typeof(MyColorEditor), typeof(UITypeEditor))]
+        public new Color SelectionForeColor
+        {
+            get { return base.SelectionForeColor; }
+            set { base.SelectionForeColor = value; }
+        }
+
+        [DisplayName("Перенос")]
+        [Description("Возвращает или задает значение, показывающее, переносится ли текстовое содержимое ячейки  объекта Таблица (DataGridView) на последующие строки или обрезается, когда оно слишком длинное и не помещается на одной строке.")]
+        [Category("Макет")]
+        [Browsable(true)]
+        public new DataGridViewTriState WrapMode
+        {
+            get { return (DataGridViewTriState)base.WrapMode; }
+            set { base.WrapMode = (System.Windows.Forms.DataGridViewTriState)value; }
+        }
+
+        [DisplayName("ЦветФона")]
+        [Description("Возвращает или задает цвет фона ячейки объекта Таблица (DataGridView).")]
+        [Category("Внешний вид")]
+        [Browsable(true)]
+        [TypeConverter(typeof(MyColorConverter))]
+        [Editor(typeof(MyColorEditor), typeof(UITypeEditor))]
+        public new Color BackColor
+        {
+            get { return base.BackColor; }
+            set { base.BackColor = value; }
+        }
+
+        [DisplayName("ЦветФонаВыделенного")]
+        [Description("Возвращает или задает цвет фона, используемый ячейкой объекта Таблица (DataGridView), когда она выбрана.")]
+        [Category("Внешний вид")]
+        [Browsable(true)]
+        [TypeConverter(typeof(MyColorConverter))]
+        [Editor(typeof(MyColorEditor), typeof(UITypeEditor))]
+        public new Color SelectionBackColor
+        {
+            get { return base.SelectionBackColor; }
+            set { base.SelectionBackColor = value; }
+        }
+
+        [DisplayName("Шрифт")]
+        [Description("Возвращает или задает шрифт, применимый к текстовому содержимому ячейки объекта Таблица (DataGridView).")]
+        [Category("Внешний вид")]
+        [Browsable(true)]
+        [TypeConverter(typeof(MyFontConverter))]
+        public new Font Font
+        {
+            get { return base.Font; }
+            set { base.Font = value; }
+        }
+
+        [DisplayName("ИмяСтиля")]
+        [Category("Прочее")]
+        [Browsable(true)]
+        [ReadOnly(true)]
+        public string NameStyle { get; set; }
+
+        [Browsable(false)]
+        public new dynamic DataSourceNullValue { get; set; }
+
+        [Browsable(false)]
+        public new dynamic Format { get; set; }
+
+        [Browsable(false)]
+        public new dynamic FormatProvider { get; set; }
+
+        [Browsable(false)]
+        public new dynamic IsDataSourceNullValueDefault { get; set; }
+
+        [Browsable(false)]
+        public new dynamic IsFormatProviderDefault { get; set; }
+
+        [Browsable(false)]
+        public new dynamic IsNullValueDefault { get; set; }
+
+        [Browsable(false)]
+        public new dynamic NullValue { get; set; }
+
+        [Browsable(false)]
+        public new dynamic Tag { get; set; }
+
+        [Browsable(false)]
+        public string DefaultValues
+        {
+            get
+            {
+                return @"
+ОсновнойЦвет == ТекстОкна
+ОсновнойЦветВыделенного == ТекстВыбранных
+ЦветФона == ЛицеваяЭлемента
+ЦветФонаВыделенного == ФонВыбранных
+Шрифт == Microsoft Sans Serif; 7,8pt
+Выравнивание == СерединаЛево
+Заполнение == 0; 0; 0; 0
+Перенос == Истина
+ИмяСтиля == Таблица1СтильЯчейки
+";
+            }
+            set { }
+        }
+
+        [Browsable(false)]
+        public string RequiredValues
+        {
+            get
+            {
+                return @"
+ИмяСтиля ==
+";
+            }
+        }
+    }
+}
---
+++ OneScriptFormsDesigner/OneScriptFormsDesigner/DateEntry.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace osfDesigner
+{
+    [DesignTimeVisible(false)]
+    public class DateEntry : Component
+    {
+        public DateEntry()
+        {
+        }
+
+        public DateEntry(DateTime p1)
+        {
+            M_DateTime = p1;
+        }
+
+        [Browsable(false)]
+        [ReadOnly(true)]
+        public DateTime M_DateTime { get; set; }
+
+        [DisplayName("Значение")]
+        [Description("Текущее время, обычно выраженное как дата и время суток.")]
+        [Category("Дата")]
+        [Browsable(true)]
+        public DateTime Value
+        {
+            get { return M_DateTime; }
+            set { M_DateTime = value; }
+        }
+    }
+}
