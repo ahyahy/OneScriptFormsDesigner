14791293863c99ea63e67b87af23fad1c3ed8d87
Vladimir Aleshechkin <min6220@yandex.ru>
2024-03-05 20:45:25 +0500

    Релиз 1_2_1_0

--- OneScriptFormsDesigner/IDesignSurfaceExt2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace osfDesigner
-{
-    public interface IDesignSurfaceExt2 : IDesignSurfaceExt
-    {
-        // Получаем IDesignerHost из .NET DesignSurface.
-        ToolboxServiceImp GetIToolboxService();
-        void EnableDragandDrop();
-    }
-}
