aac71812d9a26ff91ce15994ceb369c7bb8a623c
Vladimir Aleshechkin <min6220@yandex.ru>
2024-03-10 16:25:41 +0500

    Релиз 1_2_2_0

---
+++ OneScriptFormsDesigner/CheckState.cs
@@ -0,0 +1,9 @@
+﻿namespace osfDesigner
+{
+    public enum CheckState
+    {
+        Неопределенный = 2,
+        НеПомечен = 0,
+        Помечен = 1
+    }
+}
---
+++ OneScriptFormsDesigner/ColumnHeaderStyle.cs
@@ -0,0 +1,9 @@
+﻿namespace osfDesigner
+{
+    public enum ColumnHeaderStyle
+    {
+        Нажимаемая = 2,
+        НеНажимаемая = 1,
+        Отсутствие = 0
+    }
+}
