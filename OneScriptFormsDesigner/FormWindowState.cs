ff5a361e948e232516f194d3e0f5773dab79f9fd
Vladimir Aleshechkin <min6220@yandex.ru>
2024-03-10 16:27:10 +0500

    Релиз 1_2_2_0

---
+++ OneScriptFormsDesigner/FormWindowState.cs
@@ -0,0 +1,9 @@
+﻿namespace osfDesigner
+{
+    public enum FormWindowState
+    {
+        Развернутое = 2,
+        Свернутое = 1,
+        Стандартное = 0
+    }
+}
---
+++ OneScriptFormsDesigner/HorizontalAlignment.cs
@@ -0,0 +1,9 @@
+﻿namespace osfDesigner
+{
+    public enum HorizontalAlignment
+    {
+        Лево = 0,
+        Право = 1,
+        Центр = 2
+    }
+}
