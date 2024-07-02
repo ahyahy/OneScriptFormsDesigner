194e748d7b79beec0686208463423e5792bcdc30
Vladimir Aleshechkin <min6220@yandex.ru>
2024-07-02 21:01:16 +0500

    Релиз

---
+++ OneScriptFormsDesigner/OneScriptFormsDesigner/AnchorStyles.cs
@@ -0,0 +1,12 @@
+﻿namespace osfDesigner
+{
+    [System.Flags]
+    public enum AnchorStyles
+    {
+        Верх = 1,
+        Лево = 4,
+        Низ = 2,
+        Отсутствие = 0,
+        Право = 8
+    }
+}
