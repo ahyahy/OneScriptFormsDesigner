9fc047b7e4ba92e5489b8a66e9cde00564075d1f
Vladimir Aleshechkin <min6220@yandex.ru>
2024-07-02 21:21:18 +0500

    Релиз

---
+++ OneScriptFormsDesigner/OneScriptFormsDesigner/FormatDateTimePicker.cs
@@ -0,0 +1,11 @@
+﻿namespace osfDesigner
+{
+    [System.Flags]
+    public enum FormatDateTimePicker
+    {
+        Время = 4,
+        Длинный = 1,
+        Короткий = 2,
+        Пользовательский = 8
+    }
+}
