12a9c8d67b4f7b5f1662a38745c18c71a73f9d97
Vladimir Aleshechkin <min6220@yandex.ru>
2026-03-14 10:57:23 +0500

    Справка изменена

--- docs/OSFormsDesigner/mobilstyle.js
+++ docs/OSFormsDesigner/mobilstyle.js
@@ -19,6 +19,45 @@ else {
 	);
 }
 
+// ================================
+function divrightClick(e) {
+	if (e.target.nodeName == 'A') {
+		localStorage["destination"] = '' + e.target;
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+	localStorage["destination"] = '' + document.location;
+});
+// ================================
+// Функции копирования примеров.
+window.onload = function () {
+	var a = document.getElementById('copy1');
+	a.onclick = function () {
+		window.getSelection().removeAllRanges();
+		var range1 = document.createRange();
+		range1.selectNode(document.getElementById('cont1'));
+		try {
+			navigator.clipboard.writeText(range1);
+		} catch (err) { }
+		window.getSelection().removeRange(range1);
+		return false;
+	}
+
+	var b = document.getElementById('copy2');
+	b.onclick = function () {
+		window.getSelection().removeAllRanges();
+		var range2 = document.createRange();
+		range2.selectNode(document.getElementById('cont2'));
+		try {
+			navigator.clipboard.writeText(range2);
+		} catch (err) { }
+		window.getSelection().removeRange(range2);
+		return false;
+	}
+}
+// ================================
+
 // === Начало для масштабирования ==========
 function clickimg(e) {
 	var img01 = e.target;
